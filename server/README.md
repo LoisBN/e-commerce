6fbb8ecf63112f2fb21b63a2dc0d077421e12e06
lois <lois@pixelogi.com>
2020-03-27 23:08:29 +0100

    adding a README

---
+++ server/README.md
@@ -0,0 +1,19 @@
+# The server
+
+<ul>
+    <li>Add a config folder</li>
+    <li>Create a config.json file</li>
+    <li>Add the configuration from the config folder which is in the repo
+    </li>
+    <li>
+        Launch the server with the command ./server or ./server.linux
+    </li>
+</ul>
+
+## dependencies
+
+you have to install :
+<ul>
+    <li>mongoDB</li>
+    <li>redis</li>
+</ul>
--- server/main.go
+++ server/main.go
@@ -1,19 +1,12 @@
 package main
 
 import (
-	"fmt"
-
-	//"google.golang.org/grpc/grpclog"
-	//"google.golang.org/grpc"
-
 	"github.com/loisBN/e-commerce/server/services/auth"
 	"github.com/loisBN/e-commerce/server/services/db"
 	"github.com/loisBN/e-commerce/server/services/web"
 )
 
-func main() {
-	fmt.Println("test")
-	//auth.Test()
+func main()  {
 	async := make(chan int)
 	go web.RunAPI()
 	go db.LaunchServer()
